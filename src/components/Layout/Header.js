db733d962fc4d3c573e753bdaaa09f4ecbebe4b5
Carolina Cobo <ccobo.dev@gmail.com>
2021-11-29 11:24:44 +0000

    React meals app, cart, header, items and summary

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react/cjs/react.production.min";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
